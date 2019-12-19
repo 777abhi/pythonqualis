b7b1ae65d0fa5a3c97e481cefffea30e6dce4e51
abhinav <777abhi@gmail.com>
2019-12-19 08:05:24 +0000

    unittest 1 handson passed

--- unittest1.py
+++ unittest1.py
@@ -14,7 +14,6 @@ class Circle:
         elif self.radius > 10000 and self.radius < 0:
             raise ValueError("radius must be between 0 and 1000 inclusive")
         
-        
     def area(self):
         # Define area functionality:
         return round(math.pi * self.radius * self.radius,2)
@@ -28,7 +27,7 @@ class Circle:
 class TestCircleCreation(unittest.TestCase):
 
     def test_creating_circle_with_numeric_radius(self):
-        # Define a circle 'c1' with radius 2.5, and check if 
+       # Define a circle 'c1' with radius 2.5, and check if 
         # the value of c1.radius is equal to 2.5 or not.
         c1 = Circle(2.5)
         self.assertEqual(c1.radius,2.5)
@@ -39,6 +38,7 @@ class TestCircleCreation(unittest.TestCase):
         # if it raises a ValueError with the message
         # "radius must be between 0 and 1000 inclusive".
         c = Circle(-2.5)
+        self.assertEqual(c.radius,-2.5)
         self.assertRaises(ValueError)
         
 
@@ -47,8 +47,8 @@ class TestCircleCreation(unittest.TestCase):
         # if it raises a ValueError with the message
         # "radius must be between 0 and 1000 inclusive".    
         c = Circle(1000.1)
+        self.assertEqual(c.radius,1000.1)
         self.assertRaises(ValueError)
-            
         
 
     def test_creating_circle_with_nonnumeric_radius(self):
@@ -56,52 +56,7 @@ class TestCircleCreation(unittest.TestCase):
         # if it raises a TypeError with the message
         # "radius must be a number".        
         c = Circle(1000.1)
+        self.assertEqual(c.radius,1000.1)
         self.assertRaises(ValueError)
 
 if __name__ == '__main__':
-    
-    fptr = open('output.txt', 'w')
-    
-    runner = unittest.TextTestRunner(fptr)
-    
-    unittest.main(testRunner=runner, exit=False)
-    
-    fptr.close()
-    
-    with open('output.txt') as fp:
-        output_lines = fp.readlines()
-    
-    
-    pass_count = [ len(re.findall(r'\.', line)) for line in output_lines if line.startswith('.')
-                    and line.endswith('.\n')]
-    
-    
-    pass_count = pass_count[0]
-                       
-    print(str(pass_count))
-                       
-    doc1 = inspect.getsource(TestCircleCreation.test_creating_circle_with_numeric_radius)
-    doc2 = inspect.getsource(TestCircleCreation.test_creating_circle_with_negative_radius)
-    doc3 = inspect.getsource(TestCircleCreation.test_creating_circle_with_greaterthan_radius)
-    doc4 = inspect.getsource(TestCircleCreation.test_creating_circle_with_nonnumeric_radius)
-    
-    assert1_count = len(re.findall(r'assertEqual', doc1))
-    
-    print(str(assert1_count))
-    
-    assert1_count = len(re.findall(r'assertEqual', doc2))
-    assert2_count = len(re.findall(r'assertRaises', doc2))
-    
-    print(str(assert1_count), str(assert2_count))
-    
-    assert1_count = len(re.findall(r'assertEqual', doc3))
-    assert2_count = len(re.findall(r'assertRaises', doc3))
-    
-    print(str(assert1_count), str(assert2_count))
-    
-    assert1_count = len(re.findall(r'assertEqual', doc4))
-    assert2_count = len(re.findall(r'assertRaises', doc4))
-    
-    print(str(assert1_count), str(assert2_count))
-    
-    
