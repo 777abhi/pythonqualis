2b941bd5207f2f2c20f2f772d925bcefd72b224e
abhinav <777abhi@gmail.com>
2019-12-20 08:45:06 +0000

    nosetest saved work

--- sample_nose_test.py
+++ sample_nose_test.py
@@ -1,3 +1,38 @@
+def test_sample_nosetest():
+    assert 'HELLO' == 'hello'.upper()
+
+#python -m nose sample_nose_test.py
+#python -m nose sample_nose_test.py -v
+#nosetests  sample_nose_test.py -v
+
+def add2num(a,b):
+    return a+b
+
+def test_sum_2pos_num():
+    assert add2num(6, 7)==13
+
+def test_sum_1pos_and_1neg_num():
+    assert add2num(-10, 9)==-1
+
+
+from nose.tools import ok_, eq_
+def test_using_ok():
+    ok_(2+3==5)
+
+def test_using_eq():
+    eq_(2+3, 5)
+
+
+from nose.tools import raises
+
+@raises(TypeError)
+def test_using_raises():
+    eq_(2+'3', 5)
+
+
+#nosetests test.test_module1 -v --with-xunit
+
+
 from nose.tools import raises
 
 class SampleTestClass:
@@ -6,7 +41,7 @@ class SampleTestClass:
     def test_sample1(self):
         pow(2, '4')
 
-   @raises(Execption)
+    @raises(Execption)
     def test_sample2(self):
         max([7, 8, '4'])
 
